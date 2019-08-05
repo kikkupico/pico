1b4f37f014b2c0a010503d1eb0562edb6345fe96
Ram <ramaveer@cisco.com>
2019-08-05 11:18:42 +0530

    added creator usertype and refined as necessary

--- pico.py
+++ pico.py
@@ -5,77 +5,88 @@ app = Flask(__name__)
 db = {}
 (create, list, retrieve, update, delete) = ('create', 'list', 'retrieve', 'update', 'delete')
 
+
 class Anyone:
-    def handle_list(r):
-        return jsonify(db[r])        
+    def handle_list(r, check_request, error):
+        if check_request():
+            return jsonify(db[r])
+        else:
+            return error()
 
-    def handle_create(r):
-        db[r].append(request.json)
-        return 'created', 201
+    def handle_create(r, check_request, error):
+        if check_request():
+            creator = {'creator':request.headers['token']} if 'token' in request.headers else {'creator':'system'}
+            db[r].append({**request.json,**creator})
+            return 'created', 201
+        else:
+            return error()           
 
-    def handle_retrieve(r, i):
-        return jsonify(db[r][i])
+    def handle_retrieve(r, i, check_request, check_item, error):
+        if check_request():
+            item = db[r][i]
+            if(check_item(item)):
+                return jsonify(db[r][i])
+            else:
+                return error()
+        else:
+            return error()
 
-    def handle_update(r, i):
-        db[r][i] = {**db[r][i], **request.json}
-        return jsonify(db[r][i])
+    def handle_update(r, i, check_request, check_item, error):
+        if check_request():
+            item = db[r][i]
+            if(check_item(item)):
+                db[r][i] = {**db[r][i], **request.json}
+                return jsonify(db[r][i])            
+            else:
+                return error()            
+        else:
+            return error()
+            
+    def handle_delete(r, i, check_request, check_item, error):
+        if check_request():
+            item = db[r][i]
+            if(check_item(item)):
+                return jsonify(db[r].pop(i))
+            else:
+                return error()
+        else:
+            return error()            
+    
+    def check_request():
+        return True
 
-    def handle_delete(r, i):        
-        return jsonify(db[r].pop(i))
+    def check_item(i):
+        return True
 
-    def can(actions, resource):
-        return Anyone.override_can(Anyone, actions, resource)
+    def error():
+        return 'Not found', 404
 
-    def override_can(i, actions, resource):
+    @classmethod
+    def can(cls, actions, resource):
         if resource not in db:
             db[resource]=[]
         for action in actions:
             if action == list:
-                app.add_url_rule('/'+resource, 'list_'+resource, lambda:i.handle_list(resource))
+                app.add_url_rule('/'+resource, 'list_'+resource, lambda:cls.handle_list(resource, cls.check_request, cls.error))
             if action == create:
-                app.add_url_rule('/'+resource, 'create_'+resource, lambda:i.handle_create(resource), methods=['POST'])
+                app.add_url_rule('/'+resource, 'create_'+resource, lambda:cls.handle_create(resource, cls.check_request, cls.error), methods=['POST'])
             if action == retrieve:
-                app.add_url_rule('/'+resource+'/<int:r_id>', 'retrieve_'+resource, lambda **d: i.handle_retrieve(resource, d['r_id']))
+                app.add_url_rule('/'+resource+'/<int:r_id>', 'retrieve_'+resource, lambda **d: cls.handle_retrieve(resource, d['r_id'], cls.check_request, cls.check_item, cls.error))
             if action == update:
-                app.add_url_rule('/'+resource+'/<int:r_id>', 'update_'+resource, lambda **d: i.handle_update(resource, d['r_id']), methods=['PUT'])
+                app.add_url_rule('/'+resource+'/<int:r_id>', 'update_'+resource, lambda **d: cls.handle_update(resource, d['r_id'], cls.check_request, cls.check_item, cls.error), methods=['PUT'])
             if action == delete:
-                app.add_url_rule('/'+resource+'/<int:r_id>', 'delete_'+resource, lambda **d: i.handle_delete(resource, d['r_id']), methods=['DELETE'])
+                app.add_url_rule('/'+resource+'/<int:r_id>', 'delete_'+resource, lambda **d: cls.handle_delete(resource, d['r_id'], cls.check_request, cls.check_item, cls.error), methods=['DELETE'])
 
 
 
-class AuthenticatedUser(Anyone):
-    def can(actions, resource):
-        return Anyone.override_can(AuthenticatedUser, actions, resource)
-    
-    def handle_list(r):
-        if 'token' in request.headers:
-            return Anyone.handle_list(r)
-        else:
-            return 'forbidden', 403
+class AuthenticatedUser(Anyone):    
+    def check_request():
+        return 'token' in request.headers
 
-    def handle_create(r):
-        if 'token' in request.headers:
-            return Anyone.handle_create(r)
-        else:
-            return 'forbidden', 403
 
-    def handle_retrieve(r, i):
-        if 'token' in request.headers:
-            return Anyone.handle_retrieve(r, i)
-        else:
-            return 'forbidden', 403
-
-    def handle_update(r, i):
-        if 'token' in request.headers:
-            return Anyone.handle_update(r, i)
-        else:
-            return 'forbidden', 403
-
-    def handle_delete(r, i):        
-        if 'token' in request.headers:
-            return Anyone.handle_delete(r, i)
-        else:
-            return 'forbidden', 403
+class Creator(AuthenticatedUser):    
+    def check_item(item):
+        return request.headers['token'] == item['creator']
 
 
 @app.route('/')
--- spec.py
+++ spec.py
@@ -3,3 +3,4 @@ from pico import *
 Anyone.can([list, create],'todos')
 Anyone.can([retrieve, update, delete],'todos')
 AuthenticatedUser.can([list, create],'teams')
+Creator.can([retrieve, update, delete], 'teams')
