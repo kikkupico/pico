8cd4288bee07df1f8fdbd4b2a250cd1c7f3ca45f
Ram <kikkupico@gmail.com>
2019-08-07 03:42:50 +0530

    default props feature; fixes; can now pass todo-backend spec

--- pico.py
+++ pico.py
@@ -1,10 +1,14 @@
 from flask import Flask,jsonify,request
 from persistence import *
 from playhouse.shortcuts import model_to_dict
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 
-(create, read, update, delete) = ('create', 'read', 'update', 'delete')
+(create, read, update, delete, delete_all) = ('create', 'read', 'update', 'delete', 'delete_all')
+default_props = {}
 
 def unroll(row):
         to_dict = model_to_dict(row, recurse=False)
@@ -15,15 +19,16 @@ class Anyone:
     def handle_read_all(r, check_request, error):
         db_connection.connect()
         if check_request():            
-            return jsonify([unroll(e) for e in db[r].select()])
+            return jsonify([{**(unroll(e)), **{'url':request.url+'/'+str(e.id)}} for e in db[r].select()])
         else:
             return error()
 
     def handle_create(r, check_request, error):
         if check_request():
-            creator = User.get(token=request.headers['token']) if 'token' in request.headers else User.get(name='guest')
-            r = db[r].create(properties=request.json,creator=creator)
-            return jsonify(unroll(r)), 201
+            creator = User.get(token=request.headers['token']) if 'token' in request.headers else User.get(username='guest')
+            props = {**request.json,**default_props[r]} if default_props[r] else request.json
+            e = db[r].create(properties=props,creator=creator)
+            return jsonify(**(unroll(e)), **{'url':request.url+'/'+str(e.id)}), 201
         else:
             return error()           
 
@@ -32,7 +37,7 @@ class Anyone:
             item = db[r].get_or_none(id=i)
             if(item is None): return 'Not found', 404
             if(check_item(item)):
-                return jsonify(unroll(item))
+                return jsonify(**(unroll(item)), **{'url':request.url})
             else:
                 return error()
         else:
@@ -45,7 +50,7 @@ class Anyone:
             if(check_item(item)):
                 item.properties = {**item.properties, **request.json}
                 item.save()
-                return jsonify(unroll(item))
+                return jsonify(**(unroll(item)), **{'url':request.url})
             else:
                 return error()            
         else:
@@ -61,7 +66,14 @@ class Anyone:
             else:
                 return error()
         else:
-            return error()            
+            return error()
+
+    def handle_delete_all(r, check_request, error):
+        if check_request():            
+            db[r].delete().execute()
+            return '',204
+        else:
+            return error()
     
     def check_request():
         return True
@@ -86,6 +98,8 @@ class Anyone:
                 app.add_url_rule('/'+resource+'/<int:r_id>', 'update_'+resource, lambda **d: cls.handle_update(resource, d['r_id'], cls.check_request, cls.check_item, cls.error), methods=['PUT', 'PATCH'])
             if action == delete:
                 app.add_url_rule('/'+resource+'/<int:r_id>', 'delete_'+resource, lambda **d: cls.handle_delete(resource, d['r_id'], cls.check_request, cls.check_item, cls.error), methods=['DELETE'])
+            if action == delete_all:
+                app.add_url_rule('/'+resource, 'delete_all_'+resource, lambda:cls.handle_delete_all(resource, cls.check_request, cls.error), methods=['DELETE'])
 
 
 
--- spec.py
+++ spec.py
@@ -1,7 +1,25 @@
 from pico import *
 
-Anyone.can([create, read, update, delete],'todos')
+Anyone.can([create, read, update, delete, delete_all],'todos')
 AuthenticatedUser.can([read, create],'teams')
 Creator.can([update, delete], 'teams')
 
+default_props['todos']={'completed':False}
+
 end()
+
+
+'''
+wishlist
+
+Anyone.can([create, read, update, delete, delete_all], 'todos')
+User.can(update, 'todos', props=['completed'] )
+default_props['todos']={'completed':False})
+
+
+spec('Anyone can [create, read, update, delete] todos')
+spec('User can update completed in todos')
+spec('User can update todos')
+spec('default_properties_of todos [completed False]')
+
+'''
